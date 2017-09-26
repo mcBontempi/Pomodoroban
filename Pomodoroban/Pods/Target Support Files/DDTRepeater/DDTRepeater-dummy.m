aa122ea15bc4aa6b58d464b6830ea1c43ed26c56
Daren David Taylor <mcbontempi@gmail.com>
2017-09-26 12:01:31 +0100

    Pods removed

--- Pomodoroban/Pods/Target Support Files/DDTRepeater/DDTRepeater-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DDTRepeater : NSObject
-@end
-@implementation PodsDummy_DDTRepeater
-@end
--- Pomodoroban/Pods/Target Support Files/Pods-Tomatoe/Pods-Tomatoe-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tomatoe : NSObject
-@end
-@implementation PodsDummy_Pods_Tomatoe
-@end
