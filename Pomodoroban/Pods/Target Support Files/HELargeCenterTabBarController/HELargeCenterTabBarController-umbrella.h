d40b54d34aeffbaa7bf05761a28cb1149370020e
Daren David Taylor <mcbontempi@gmail.com>
2017-09-26 10:25:26 +0100

    removed Pods;

--- Pomodoroban/Pods/Target Support Files/HELargeCenterTabBarController/HELargeCenterTabBarController-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "HELargeCenterTabBarController.h"
-
-FOUNDATION_EXPORT double HELargeCenterTabBarControllerVersionNumber;
-FOUNDATION_EXPORT const unsigned char HELargeCenterTabBarControllerVersionString[];
-
