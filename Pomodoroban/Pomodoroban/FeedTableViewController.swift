c0e8c164e5268e023d2747541bf89a9a2053c3ed
Daren David Taylor <mcbontempi@gmail.com>
2017-10-23 15:54:39 +0100

    removed pod compiler warnings

--- Pomodoroban/Pomodoroban/FeedTableViewController.swift
+++ Pomodoroban/Pomodoroban/FeedTableViewController.swift
@@ -1,50 +1,35 @@
 
 import UIKit
 
-enum ESection {
-    case user
+enum ESection : Int{
+    case user = 0
     case alert
     case create
     case session
     case archive
     case preferences
+    case sectionCount
 }
 
-enum EField {
-    case userName
-    case alertTitle
-}
 
 class FeedTableViewController: UITableViewController {
 
-    func sections() ->  [ESection: [ [EField:String]  ]   ]  {
-        
-        return [
-                        ESection.user:[[ EField.userName : "daren"]],
-                        ESection.alert:[
-                            [ EField.alertTitle : "Well pomodoroed you have done 5 days in a row."  ],
-                            [ EField.alertTitle : "Have you thought about upgrading to Pomodoroban 3.0"  ]
-                        ]
-                     ]
-    }
-
-    func rowsForIndexPath(indexPath:IndexPath) ->  [ [ [EField:String]  ]
-    {
-        let section =
-        
-        return self.sections()[
-    }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userHeader")
+        
+        return cell!
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-    
+        return ESection.sectionCount.rawValue
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
         
     }
 
