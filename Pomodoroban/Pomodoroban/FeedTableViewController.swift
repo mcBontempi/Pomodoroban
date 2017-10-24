44e50bd43dfe6cb5a48f1d3f7c05dc6069499cdf
Daren David Taylor <mcbontempi@gmail.com>
2017-10-24 08:05:43 +0100

    starting going through the cells on the feed

--- Pomodoroban/Pomodoroban/FeedTableViewController.swift
+++ Pomodoroban/Pomodoroban/FeedTableViewController.swift
@@ -18,18 +18,21 @@ class FeedTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "userHeader")
+        let identifier = ["userHeader","userSwipe","alertHeader","alert","createHeader","createSwipe","sessionHeader","session","archiveHeader","archive","preferencesHeader","preferences","companyDetails"][indexPath.row]
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier)
         
         return cell!
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return ESection.sectionCount.rawValue
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 1
+        return 13
         
     }
 
---
+++ Pomodoroban/Pomodoroban/UserHeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserHeaderTableViewCell.swift
+//  efficacious
+//
+//  Created by mcBontempi on 24/10/2017.
+//  Copyright © 2017 LondonSwift. All rights reserved.
+//
+
+import UIKit
+
+class UserHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
